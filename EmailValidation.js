3c0da817aef4523a6ee86d45fed344f33061f0aa
swatikanishu <nishuswatika@gmail.com>
2022-08-04 02:14:45 +0530

    REFACTORED:Regex to ensure 2 character country code

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,18 +1,88 @@
 console.log("***** Welcome To Email Validation *****");
 
-const emailRegex = RegExp("[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[.a-z]*");
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
-function validateEmail(email){
-    if(emailRegex.test(email))
+function validateEmail(email) {
+    if (emailRegex.test(email))
         console.log(email + " : Valid EMAIL !!");
     else
-        throw email + " : EMAIL is Invalid !";
+        console.log(email + " : EMAIL is Invalid !");
 }
 
-try{
-    validateEmail("abc.ash@bridgelabz.co.in");
-    validateEmail("abc()*@gmail.com");
-}
-catch(e){
-    console.error(e);
-}
+console.log("---------- Valid Emails ----------");
+		
+let email = "abc@bridgelabz.co.in";
+validateEmail(email);
+
+email = "abc@gmail.com.com";
+validateEmail(email);
+
+email = "abc@yahoo.com";
+validateEmail(email);
+
+email = "abc@1.com";
+validateEmail(email);
+
+email = "abc-100@yahoo.com";
+validateEmail(email);
+
+email = "abc.100@yahoo.com";
+validateEmail(email);
+
+email = "abc111@abc.com";
+validateEmail(email);
+
+email = "abc-100@abc.net";
+validateEmail(email);
+
+email = "abc.100@abc.com.au";
+validateEmail(email);
+
+email = "abc+100@gmail.com";
+validateEmail(email);
+
+console.log("---------------------------------------------------");
+
+console.log("---------- Invalid Emails ----------");
+
+email = ".abc@abc.com";
+validateEmail(email);
+
+email = "abc";
+validateEmail(email);
+
+email = "abc@.com.my";
+validateEmail(email);
+
+email = "abc@abc@gmail.com";
+validateEmail(email);
+
+email = "abc()*@gmail.com";
+validateEmail(email);
+
+email = "abc..2002@gmail.com";
+validateEmail(email);
+
+email = "abc.@gmail.com";
+validateEmail(email);
+
+email = "abc123@.com";
+validateEmail(email);
+
+email = "abc123@.com.com";
+validateEmail(email);
+
+email = "abc123@gmail.a";
+validateEmail(email);
+
+email = "abc@%*.com";
+validateEmail(email);
+
+email = "abc@gmail.com.1a";
+validateEmail(email);
+
+email = "abc@gmail.com.aa.au";
+validateEmail(email);
+
+console.log("---------------------------------------------------");
+
